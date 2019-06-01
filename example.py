dd4e61225a31a5cff49764c5cae191fa5e0469ca
dave <dave.berenbaum@gmail.com>
2019-05-31 21:06:31 -0400

    update cli scripts

--- example.py
+++ example.py
@@ -1,20 +1,32 @@
 import random
 
+import click
+
 import core
 import human_play
 import monte_carlo
 import mongo_serialize
 
-with mongo_serialize.MongoTree("mongodb://localhost:9999/") as tree:
-    x = human_play.HumanPlayer("x", (core.Pawn(), core.Pawn()))
-    o = monte_carlo.MonteCarloPlayer("o", (core.Pawn(), core.Pawn()), tree)
-    print("You are player x.")
-    players = [x, o]
-    random.shuffle(players)
-    game = core.Game(players)
-    print("Wait for data to load.")
-    print("Player {:s} starts".format(str(players[0])))
-    game.print_state()
-    for player in game.play():
+
+CONN_STR = "mongodb://localhost:27017/"
+
+@click.command()
+@click.option("-c", "conn_str", default=CONN_STR)
+def play(conn_str=CONN_STR):
+    with mongo_serialize.MongoTree(conn_str) as tree:
+        x = human_play.HumanPlayer("x", (core.Pawn(), core.Pawn()))
+        o = monte_carlo.MonteCarloPlayer("o", (core.Pawn(), core.Pawn()), tree)
+        print("You are player x.")
+        players = [x, o]
+        random.shuffle(players)
+        game = core.Game(players)
+        print("Wait for data to load.")
+        print("Player {:s} starts".format(str(players[0])))
         game.print_state()
-    print("Player {:s} has won!".format(str(game.winner())))
+        for player in game.play():
+            game.print_state()
+        print("Player {:s} has won!".format(str(game.winner())))
+
+
+if __name__ == '__main__':
+    play()
--- monte_carlo_iters.py
+++ monte_carlo_iters.py
@@ -1,5 +1,7 @@
 import sys
 
+import click
+
 import core
 import monte_carlo
 import random
@@ -7,13 +9,20 @@ import serialize
 import mongo_serialize
 
 
-n = int(sys.argv[1])  # Number of games to simulate.
+CONN_STR = "mongodb://localhost:27017/"
 
-with mongo_serialize.MongoTree("mongodb://localhost:9999/") as tree:
-    x = monte_carlo.MonteCarloPlayer("x", (core.Pawn(), core.Pawn()), tree)
-    o = monte_carlo.MonteCarloPlayer("o", (core.Pawn(), core.Pawn()), tree)
-    players = [x, o]
-    random.shuffle(players)
-    game = core.Game(players)
-    for i, _ in enumerate(serialize.repeat_plays(game, tree, n=n)):
-        print("%d games played" % i)
+@click.command()
+@click.argument("n", type=int)
+def play(n, conn_str=CONN_STR):
+    with mongo_serialize.MongoTree(conn_str) as tree:
+        x = monte_carlo.MonteCarloPlayer("x", (core.Pawn(), core.Pawn()), tree)
+        o = monte_carlo.MonteCarloPlayer("o", (core.Pawn(), core.Pawn()), tree)
+        players = [x, o]
+        random.shuffle(players)
+        game = core.Game(players)
+        for i, _ in enumerate(serialize.repeat_plays(game, tree, n=n)):
+            print("%d games played" % i)
+
+
+if __name__ == '__main__':
+    play()
